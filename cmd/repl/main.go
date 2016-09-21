922f1b1b4a53d9d013c9919856e5fd94d2178bb8
Michael Henry <michael.henry@dealer.com>
2016-09-20 21:42:01 -0400

    don't kill repl on eval error

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -5,26 +5,39 @@ import "os"
 import "bufio"
 import "strings"
 import "goober-lisp/goober"
+import "runtime/debug"
 
 func isEmpty(s string) bool {
 	t := strings.TrimSpace(s)
 	return len(t) == 0
 }
 
+func handle(globalVars map[string]goober.Value, input string) {
+
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("%s: %s", e, debug.Stack())
+		}
+	}()
+
+	if !isEmpty(input) {
+		value := goober.Read(input)
+		fmt.Printf("%v\n", goober.Eval(globalVars, *value))
+	}
+}
+
 func main() {
 	globalVars := make(map[string]goober.Value)
 	for {
+
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("goober-lisp> ")
 
-		text, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
 
-		if !isEmpty(text) {
-			value := goober.Read(text)
-			fmt.Printf("%v\n", goober.Eval(globalVars, *value))
-		}
+		handle(globalVars, input)
 	}
 }

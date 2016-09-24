060a7d7480e8e356f2e56ebf9be1ef6bf04d67b4
Michael Henry <froderick@gmail.com>
2016-09-24 17:42:17 -0400

    mid-rewrite

--- goober/eval.go
+++ goober/eval.go
@@ -3,22 +3,48 @@ package goober
 import "math"
 import "fmt"
 
+// incorporate functions as value types
+
+type fn struct {
+	args  []Symbol
+	exprs []Value
+}
+
+func (v fn) truthy() bool {
+	return true
+}
+
+func (v fn) prn() string {
+	return fmt.Sprintf("#fn[%v]", v)
+}
+
+// data structures to support vars and bindings
+
+type ns struct {
+	name string
+	vars map[string]Value
+}
+
+func (ns *ns) def(name string, value Value) {
+	ns.vars[name] = value
+}
+
+func (ns *ns) undef(name string) {
+	delete(ns.vars, name)
+}
+
 type binding struct {
-	Name  string
-	Value Value
+	name  string
+	value Value
 }
 
 type context struct {
-	globalVars map[string]Value
-	bindings   []binding
+	ns       *ns
+	bindings []binding
 }
 
-func (c *context) def(name string, value Value) {
-	c.globalVars[name] = value
-}
-
-func (c *context) push(name string, value Value) {
-	b := binding{Name: name, Value: value}
+func (c *context) push(name Symbol, value Value) {
+	b := binding{name: string(name), value: value}
 	c.bindings = append(c.bindings, b)
 }
 
@@ -34,19 +60,21 @@ func (c *context) pop() binding {
 	return popped
 }
 
-func (c context) get(name string) Value {
+func (c context) get(name Symbol) Value {
+
+	s := string(name)
 
 	if len(c.bindings) > 0 {
 		for i := len(c.bindings); i > 0; i-- {
 			binding := c.bindings[i-1]
-			if name == binding.Name {
-				return binding.Value
+			if s == binding.name {
+				return binding.value
 			}
 		}
 	}
 
-	if globalVar, ok := c.globalVars[name]; ok {
-		return globalVar
+	if v, ok := c.ns.vars[s]; ok {
+		return v
 	}
 
 	panic("cannot find a binding or var with this symbol name: " + name)
@@ -58,48 +86,77 @@ func (c context) get(name string) Value {
 func special_def(context *context, vals []Value) Value {
 
 	if len(vals) != 2 {
-		panic("def takes only 2 parameters: " + Value{List: vals}.String())
+		panic(fmt.Sprintf("def takes only 2 parameters: %v", vals))
 	}
 
-	varName := vals[0]
-	if !varName.isSymbol() {
-		panic("vars can only be named by symbols: " + varName.String())
+	switch varname := vals[0].(type) {
+	case Symbol:
+		context.ns.def(string(varname), eval(context, vals[1]))
+	default:
+		panic(fmt.Sprintf("vars can only be named by symbols: %v", varname))
 	}
 
-	context.def(*varName.Symbol, eval(context, vals[1]))
+	return Nil{}
+}
 
-	return Value{}
+func requireSymbol(v Value, msg string) Symbol {
+	switch x := v.(type) {
+	case Symbol:
+		return x
+	default:
+		panic(fmt.Sprintf(msg+": %v", v))
+	}
+}
+
+func requireInt(v Value, msg string) Int {
+	switch x := v.(type) {
+	case Int:
+		return x
+	default:
+		panic(fmt.Sprintf(msg+": %v", v))
+	}
+}
+
+func requireSexpr(v Value, msg string) Sexpr {
+	switch x := v.(type) {
+	case Sexpr:
+		return x
+	default:
+		panic(fmt.Sprintf(msg+": %v", v))
+	}
+}
+
+func requireFn(v Value, msg string) fn {
+	switch x := v.(type) {
+	case fn:
+		return x
+	default:
+		panic(fmt.Sprintf(msg+": %v", v))
+	}
 }
 
 func special_let(context *context, vals []Value) Value {
 
 	if len(vals) < 1 {
-		panic("let takes at least 1 parameter: " + Value{List: vals}.String())
+		panic(fmt.Sprintf("let takes at least 1 parameter: %v", vals))
 	}
 
-	bindings := vals[0]
-	if !bindings.isList() {
-		panic("let's first argument must be a list: " + bindings.String())
-	}
+	bindings := requireSexpr(vals[0], "vars can only be named by symbols")
 
-	if math.Mod(float64(len(bindings.List)), 2) != 0 {
-		panic("let's binding list must be an even number of values: " + bindings.String())
+	if math.Mod(float64(len(bindings)), 2) != 0 {
+		panic(fmt.Sprintf("let's binding list must be an even number of values: %v", bindings))
 	}
 
 	pushes := 0
 
 	// eval each binding, add it to the context
-	for i := 0; i < len(bindings.List); i += 2 {
+	for i := 0; i < len(bindings); i += 2 {
 
-		bindingName := bindings.List[i]
-		if !bindingName.isSymbol() {
-			panic("bindings can only be made for symbols: " + bindingName.String())
-		}
+		sym := requireSymbol(bindings[i], "bindings can only be made for symbols")
+		expr := bindings[i+1]
+		val := eval(context, expr)
 
-		bindingExpr := bindings.List[i+1]
-		bindingValue := eval(context, bindingExpr)
-
-		context.push(*bindingName.Symbol, bindingValue)
+		context.push(sym, val)
 
 		pushes++
 	}
@@ -126,69 +183,66 @@ func special_let(context *context, vals []Value) Value {
 func special_if(context *context, vals []Value) Value {
 
 	if len(vals) < 2 {
-		panic("if takes at least 2 parameters: " + Value{List: vals}.String())
+		panic(fmt.Sprintf("if takes at least 2 parameters: %v", vals))
 	}
 
 	if len(vals) > 3 {
-		panic("if takes at most 3 parameters: " + Value{List: vals}.String())
+		panic(fmt.Sprintf("if takes at most 3 parameters: %v", vals))
 	}
 
 	test := eval(context, vals[0])
 
-	if test.truthy() {
-		return eval(context, vals[1])
-	} else {
-		if len(vals) == 2 {
-			return Value{}
+	switch v := test.(type) {
+	case Value:
+		if v.truthy() {
+			return eval(context, vals[1])
 		} else {
-			return eval(context, vals[2])
+			if len(vals) == 2 {
+				return Nil{}
+			} else {
+				return eval(context, vals[2])
+			}
 		}
+	default:
+		panic(fmt.Sprintf("only value types can be tested for truthiness: %v", test))
 	}
 }
 
-func special_fn(vals []Value) Value {
+func special_fn(vals []Value) fn {
 
 	if len(vals) < 2 {
-		panic("fn takes least 2 parameters: " + Value{List: vals}.String())
+		panic(fmt.Sprintf("fn takes at least 2 parameters: %v", vals))
 	}
 
-	rawParams := vals[0]
-	if !rawParams.isList() {
-		panic("expected args in the form of a list: " + rawParams.String())
+	params := requireSexpr(vals[0], "expected args in the form of a list")
+
+	names := make([]Symbol, 0, len(params))
+	for i := range params {
+		name := requireSymbol(params[i], "arguments to functions must be symbols")
+		names = append(names, name)
 	}
 
-	paramNames := make([]string, 0, len(rawParams.List))
-	for i := range rawParams.List {
-
-		name := rawParams.List[i]
-		if !name.isSymbol() {
-			panic("arguments to functions must be symbols: " + name.String())
-		}
-
-		paramNames = append(paramNames, *name.Symbol)
-	}
-
-	return Value{Fn: &Fn{Args: paramNames, Statements: vals[1:]}}
+	return fn{args: names, exprs: vals[1:]}
 }
 
-func special_fn_call(name string, fn Fn, context *context, vals []Value) Value {
+func special_fn_call(name string, fn fn, context *context, vals []Value) Value {
 
-	if len(vals) < len(fn.Args) {
-		panic(fmt.Sprintf("%v takes %v parameters: %v", name, len(fn.Args), Value{List: vals}.String()))
+	if len(vals) < len(fn.args) {
+		panic(fmt.Sprintf("%v takes %v parameters: %v", name, len(fn.args), vals))
 	}
 
-	for i, bindingName := range fn.Args {
+	for i, bindingname := range fn.args {
 		bindingExpr := vals[i]
 		bindingValue := eval(context, bindingExpr)
-		context.push(bindingName, bindingValue)
+		context.push(bindingname, bindingValue)
 	}
 
 	var result Value
-	for _, expr := range fn.Statements { // TODO: these are not statements, they are expressions
+	for _, expr := range fn.exprs {
 		result = eval(context, expr)
 	}
 
-	for range fn.Args {
+	for range fn.args {
 		context.pop() // TODO: these cleanups should happen even if evaulation fails
 	}
 
@@ -197,108 +251,102 @@ func special_fn_call(name string, fn Fn, context *context, vals []Value) Value {
 
 func special_quote(vals []Value) Value {
 	if len(vals) != 1 {
-		panic(fmt.Sprintf("quote takes only 1 parameter: %v", Value{List: vals}))
+		panic(fmt.Sprintf("quote takes only 1 parameter: %v", vals))
 	}
 	param := vals[0]
 	return param
 }
 
-func builtin_plus(vals []Value) Value {
+func builtin_plus(vals []Value) Int {
 	var base int = 0
 	for i := range vals {
-		val := vals[i]
-		if !val.isNumber() {
-			panic("arguments to '+' must be numbers: " + val.String())
-		}
-		base = base + *val.Number
+		val := requireInt(vals[i], "arguments to '+' must be numbers")
+		base = base + int(val)
 	}
-	return Value{Number: &base}
+	return Int(base)
 }
 
-func evalArgs(context *context, v Value) []Value {
-	evaluatedArgs := make([]Value, 0, len(v.List)-1)
-	for i := 1; i < len(v.List); i++ {
-		elem := eval(context, v.List[i])
-		evaluatedArgs = append(evaluatedArgs, elem)
+func evalRest(context *context, v Sexpr) []Value {
+	rest := make([]Value, 0, len(v)-1)
+	for _, item := range v[1:] {
+		evaluated := eval(context, item)
+		rest = append(rest, evaluated)
 	}
-	return evaluatedArgs
+	return rest
+}
+
+func evalSexpr(context *context, v Sexpr) Value {
+
+	if len(v) == 0 {
+		return v
+	}
+
+	switch first := v[0].(type) {
+
+	case Sexpr:
+		resolved := make([]Value, 0)
+		resolved = append(resolved, eval(context, first))
+		resolved = append(resolved, v[1:]...)
+		return evalSexpr(context, Sexpr(resolved))
+
+	case fn:
+		return special_fn_call("anonymous", first, context, evalRest(context, v))
+
+	case Symbol:
+
+		// special functions
+
+		rawArgs := v[1:]
+
+		// TODO: by putting these into a map of name -> fn(context, args)
+		// we would make these resolvable as symbols as well as functions
+		switch first {
+		case "def":
+			return special_def(context, rawArgs)
+		case "let":
+			return special_let(context, rawArgs)
+		case "if":
+			return special_if(context, rawArgs)
+		case "fn":
+			return special_fn(rawArgs)
+		case "quote":
+			return special_quote(rawArgs)
+		case "+":
+			return builtin_plus(evalRest(context, v))
+		}
+
+		// bound functions
+
+		resolved := context.get(first)
+		fn := requireFn(resolved, "symbol must be bound to a function")
+		return special_fn_call(string(first), fn, context, evalRest(context, v))
+	default:
+		panic(fmt.Sprintf("not a valid function: %", first))
+	}
 }
 
 // Evaluates a Value data structure as code.
 func eval(context *context, v Value) Value {
-
-	if v.isList() {
-
-		if len(v.List) == 0 {
-			return v
+	switch v := v.(type) {
+	case Sexpr:
+		return evalSexpr(context, v)
+	case Symbol:
+		if "nil" == string(v) {
+			return Nil{}
 		}
-
-		fn := v.List[0]
-
-		if fn.isList() {
-			fn = eval(context, fn)
-		}
-
-		if fn.isFn() {
-			return special_fn_call("anonymous", *fn.Fn, context, evalArgs(context, v))
-		}
-
-		if fn.isSymbol() {
-
-			sym := *fn.Symbol
-
-			// special functions
-
-			rawArgs := v.List[1:]
-
-			// TODO: by putting these into a map of name -> fn(context, args)
-			// we would make these resolvable as symbols as well as functions
-			switch sym {
-			case "def":
-				return special_def(context, rawArgs)
-			case "let":
-				return special_let(context, rawArgs)
-			case "if":
-				return special_if(context, rawArgs)
-			case "fn":
-				return special_fn(rawArgs)
-			case "quote":
-				return special_quote(rawArgs)
-			case "+":
-				return builtin_plus(evalArgs(context, v))
-			}
-
-			// bound functions
-
-			resolved := context.get(sym)
-
-			if !resolved.isFn() {
-				panic("symbol is not bound to a function: " + sym)
-			}
-
-			return special_fn_call(sym, *resolved.Fn, context, evalArgs(context, v))
-		}
-
-		panic("function names must be symbols: " + fn.String())
+		return context.get(v)
+	default:
+		return v
 	}
-
-	if v.isSymbol() {
-
-		if "nil" == *v.Symbol {
-			return Value{}
-		}
-
-		return context.get(*v.Symbol)
-	}
-
-	return v
 }
 
 func Eval(globalVars map[string]Value, v Value) Value {
 
+	ns := ns{name: "user", vars: map[string]Value{}}
+
 	context := context{
-		globalVars: globalVars,
-		bindings:   make([]binding, 0),
+		ns:       &ns,
+		bindings: make([]binding, 0),
 	}
 
 	return eval(&context, v)

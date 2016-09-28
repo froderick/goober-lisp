dff395b23ad553bb7726fc4897bdc3aac60b1dca
Michael Henry <michael.henry@dealer.com>
2016-09-27 22:53:18 -0400

    got shell script working

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -6,6 +6,7 @@ import "bufio"
 import "strings"
 import "goober-lisp/goober"
 import "runtime/debug"
+import "io/ioutil"
 
 func isEmpty(s string) bool {
 	t := strings.TrimSpace(s)
@@ -30,16 +31,51 @@ func handle(ns *goober.Ns, input string) {
 
 func main() {
 	ns := goober.DefaultNs()
-	for {
 
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(ns.Name + "> ")
+	stat, _ := os.Stdin.Stat()
 
-		input, err := reader.ReadString('\n')
+	if len(os.Args) > 1 { // read from file
+
+		data, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
-			break
+			panic(fmt.Sprintf("error reading input", err))
 		}
 
-		handle(ns, input)
+		lines := strings.Split(string(data), "\n")
+		filtered := make([]string, 0)
+		for _, l := range lines {
+			if !strings.HasPrefix(l, "#!") {
+				filtered = append(filtered, l)
+			}
+		}
+		done := strings.Join(filtered, "\n")
+
+		for _, val := range goober.Read(done) {
+			goober.Eval(ns, val)
+		}
+
+	} else if (stat.Mode() & os.ModeCharDevice) == 0 { // handle piped lisp script
+
+		reader := bufio.NewReader(os.Stdin)
+		data, err := ioutil.ReadAll(reader)
+		if err != nil {
+			panic(fmt.Sprintf("error reading input", err))
+		}
+
+		for _, val := range goober.Read(string(data)) {
+			goober.Eval(ns, val)
+		}
+	} else { // fall back to repl
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print(ns.Name + "> ")
+
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+
+			handle(ns, input)
+		}
 	}
 }

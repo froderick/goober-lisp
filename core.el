0a0f622cf87fe40e277c70aa796dbfc4b99303b0
Michael Henry <michael.henry@dealer.com>
2016-09-28 00:30:49 -0400

    got map working

--- core.el
+++ core.el
@@ -1,1 +1,27 @@
 (def not (fn (x) (if x false true)))
+
+(def empty? (fn (coll) (= (count coll) 0)))
+
+(def inc (fn (n) (+ n 1)))
+
+(def reverse 
+  (fn (coll)
+    (let (map-inner (fn (old-coll new-coll)
+                      (if (empty? old-coll)
+                        new-coll
+                        (recur
+                          (rest old-coll)
+                          (cons (first old-coll) new-coll)))))
+      (map-inner coll (list)))))
+
+(def map 
+  (fn (f coll)
+    (let (map-inner (fn (old-coll new-coll)
+                      (if (empty? old-coll)
+                        new-coll
+                        (recur
+                          (rest old-coll)
+                          (cons (f (first old-coll)) new-coll))))
+          mapped (map-inner (seq coll) (list)))
+      (reverse mapped))))
+
